a168bd4673b89ad1f53fef86ce3b5aa1cdc324cc
oriol <oriol.casals.7e6@itb.cat>
2023-05-04 09:32:01 +0200

    Games

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,34 @@
-listeners = ["jugar", "stats", "ranking", "imagenUsuario", "menu"]
-listeners.forEach(listener => {document.getElementById(listener).addEventListener("click", function () { test() }, false)});
+listeners = ["jugar", "stats", "ranking", "imagenUsuario", "menu"];
+listeners.forEach(listener => {document.getElementById(listener).addEventListener("click", function () { changeWindow(this) }, false)});
 
-function test() {
-    console.log("clicked")
+
+// Changes the DOM window with new content.
+function changeWindow(ele) {
+    if (!listeners.includes(ele.id)) return
+    let gameArea = document.getElementById("juego");
+
+    // Switch case for button presses.
+    switch (ele.id) {
+        case "jugar":
+            // Setup the new iframe for the game.
+            var game = document.createElement("iframe");
+            game.classList += "fullscreen";
+            game.style.border = "none";
+            game.src = "/games/jumpTheCar.html"; // game will need to be set via api request later on.
+
+            // Edit DOM.
+            document.getElementById("botones").style.display = "none";
+            gameArea.appendChild(game);
+            break;
+
+        case "menu":
+            console.log(ele);
+            let subMenu = document.getElementsByClassName('sub-menu');
+            console.log(subMenu[0]);
+            subMenu[0].style.display = 'block';
+            break;
+    
+        default:
+            break;
+    }
 }
